0fd0e580b84a74f973f4b4727b551ef999445292
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2019-05-06 16:10:07 +0530

    destructure props in the function body

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Greet = ({name, heroName}) => {
+const Greet = (props) => {
+    const { name, heroName } = props;
     return (
         <div>
             <h2>Hello {name} a.k.a {heroName}</h2>
