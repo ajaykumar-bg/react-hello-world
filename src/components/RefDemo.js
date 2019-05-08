f781a7b8f51e4124aca32e00699a8a3e2f1772eb
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2019-05-08 10:33:38 +0530

    Ref - using callback ref

--- src/components/RefDemo.js
+++ src/components/RefDemo.js
@@ -3,22 +3,30 @@ import React, { Component } from 'react'
 class RefDemo extends Component {
     constructor(props) {
       super(props)
-      this.inputRef = React.createRef();
+      // this.inputRef = React.createRef();
+      this.cbRef = null;
+      this.setCBRef = element => {
+          this.cbRef = element;
+      }
     }
 
     componentDidMount() {
-        console.log(this.inputRef);
-        this.inputRef.current.focus();
+        // console.log(this.inputRef);
+        // this.inputRef.current.focus();
+        console.log(this.cbRef);
+        this.cbRef.focus();
     }
 
     clickHandler = () => {
-        alert(this.inputRef.current.value);
+        // alert(this.inputRef.current.value);
+        alert(this.cbRef.value);
     }
     
   render() {
     return (
       <div>
-        <input type="text" placeholder="User name" ref={this.inputRef}></input>
+          <input type="text" placeholder="User name" ref={this.setCBRef}></input>
+        {/* <input type="text" placeholder="User name" ref={this.inputRef}></input> */}
         <button onClick={this.clickHandler}>Click</button>
       </div>
     )
